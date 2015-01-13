ad14c0f20ed0af005b3b473006b1bc389f1f7bfa
BH_Lin <binghuanlin@gmail.com>
2015-01-14 01:45:12 +0800

    first check-in

---
+++ javascripts/selfiestatiion.js
@@ -0,0 +1,99 @@
+console.log("running ~~");
+
+function calculateDistance(lat1, lon1, lat2, lon2) {
+  var R = 6371; // km
+  var dLat = (lat2-lat1) * Math.PI / 180;
+  var dLon = (lon2-lon1) * Math.PI / 180;
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+          Math.sin(dLon/2) * Math.sin(dLon/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c;
+  return Math.round(d*1000,10);
+}
+
+var storeDataArray = [];
+var currentGeolocation;
+
+function sortDistance(a,b)
+{
+    return a["distance"] - b["distance"];
+}
+
+angular.module('ipartcontacts', ['ngMaterial'])
+.config( [
+    '$compileProvider',
+    function( $compileProvider )
+    {
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|tel|mailto|skype|chrome-extension):/);
+        // Angular before v1.2 uses $compileProvider.urlSanitizationWhitelist(...)
+    }
+]).controller('AppCtrl', function($scope) {
+
+	console.log("checkpoint6");
+
+    //$scope.todos =contactsJsonData;
+    //console.log($scope.todos);
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position){
+
+        console.log("get geolocation: ", position);
+
+        for(var i =0; i< importedDataArray.length; i++) {
+          importedDataArray[i].distance = calculateDistance(
+            position.coords.latitude, position.coords.longitude,
+            importedDataArray[i].latitude, importedDataArray[i].longitude);
+        }
+
+        importedDataArray.sort(sortDistance);
+
+        $scope.$apply(function(){
+          $scope.kireiplaces = importedDataArray;
+        });
+    });
+  }
+
+    /*
+    $scope.todos = [
+      {
+        face : 'images/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : 'images/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : 'images/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : 'images/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        face : 'images/60.jpeg',
+        what: 'Brunch this weekend?',
+        who: 'Min Li Chan',
+        when: '3:08PM',
+        notes: " I'll be in your neighborhood doing errands"
+      },
+    ]
+    */
+
+});
+
+console.log("done ~~");
